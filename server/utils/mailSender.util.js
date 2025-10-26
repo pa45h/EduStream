492b774575d89cd76570cceafa11712530e4b7c8
Parth Katariya <parth.katariya87@gmail.com>
2025-10-27 01:55:25 +0530

    smtp

--- server/utils/mailSender.util.js
+++ server/utils/mailSender.util.js
@@ -5,6 +5,8 @@ const mailSender = async (email, title, body) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
