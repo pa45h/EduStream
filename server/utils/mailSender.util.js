e296062bcf09b13f2cbab4e5cf0ba49785ae8692
Parth Katariya <parth.katariya87@gmail.com>
2025-10-29 22:22:53 +0530

    email-port 2525

--- server/utils/mailSender.util.js
+++ server/utils/mailSender.util.js
@@ -5,7 +5,7 @@ const mailSender = async (email, title, body) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp-relay.brevo.com",
-      port: 587,
+      port: 2525,
       secure: false,
       auth: {
         user: process.env.BREVO_SMTP_LOGIN,
