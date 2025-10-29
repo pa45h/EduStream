42c18f4fed92fa53c79cc13a607307203ed1820e
Parth Katariya <parth.katariya87@gmail.com>
2025-10-29 21:50:50 +0530

    brevo dlt

--- server/utils/mailSender.util.js
+++ server/utils/mailSender.util.js
@@ -1,26 +1,7 @@
-// const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
-// const { Resend } = require("resend");
-// const resend = new Resend(process.env.RESEND_API_KEY);
-import { CreateContact, ContactsApi } from "@getbrevo/brevo";
-import { TransactionalEmailsApi, SendSmtpEmail } from "@getbrevo/brevo";
-
-const CreateContactFunc = async (email) => {
-  let contactAPI = new ContactsApi();
-  contactAPI.authentications.apiKey.apiKey = process.env.BREVO_API_KEY;
-
-  let contact = new CreateContact();
-  contact.email = email;
-
-  contactAPI
-    .createContact(contact)
-    .then((res) => {
-      console.log(JSON.stringify(res.body));
-    })
-    .catch((err) => {
-      console.error("Error creating contact:", err.body);
-    });
-};
+const { Resend } = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const mailSender = async (email, title, body) => {
   try {
@@ -32,42 +13,16 @@ const mailSender = async (email, title, body) => {
     //   },
     // });
 
-    CreateContactFunc(email);
-
-    let emailAPI = new TransactionalEmailsApi();
-    emailAPI.authentications.apiKey.apiKey = process.env.BREVO_SMTP_PASSWORD;
-
-    // const mailOptions = {
-    //   from: `EduStream - Parth Katariya`,
-    //   to: `${email}`,
-    //   subject: `${title}`,
-    //   html: `${body}`,
-    // };
-
-    let message = new SendSmtpEmail();
-    message.subject = `${title}`;
-    message.htmlContent = `${body}`;
-    message.sender = {
-      name: "EduStream - Parth Katariya",
-      email: "pa45h.katariya@gmail.com",
+    const mailOptions = {
+      from: `EduStream - Parth Katariya <onboarding@resend.dev>`,
+      to: `${email}`,
+      subject: `${title}`,
+      html: `${body}`,
     };
-    message.to = [{ email: email }];
-
-    const info = emailAPI.sendTransacEmail(message);
-
-    // const transporter = nodemailer.createTransport({
-    //   host: "smtp-relay.sendinblue.com", // Brevo SMTP Host
-    //   port: 587, // Or 465 with secure: true
-    //   secure: false, // Use TLS
-    //   auth: {
-    //     user: process.env.BREVO_SMTP_LOGIN, // Your Brevo Username
-    //     pass: process.env.BREVO_SMTP_PASSWORD, // Your Brevo API Key/Password
-    //   },
-    // });
 
     // const info = await transporter.sendMail(mailOptions);
 
-    // const info = await resend.emails.send(mailOptions);
+    const info = await resend.emails.send(mailOptions);
 
     console.log("✅ Email sent:", info);
     return info;
